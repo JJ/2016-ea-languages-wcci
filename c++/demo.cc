47436142cb474adff782dc50dc783653f23c1b16
gustavo <gustavo@ugr.es>
2015-11-04 16:48:12 +0100

    fixing small bug in c++ example

--- c++/demo.cc
+++ c++/demo.cc
@@ -11,12 +11,13 @@ int main()
 {
     std::random_device rd;
     std::default_random_engine engine(rd());
-    std::uniform_int_distribution<unsigned> distribution(0, LENGTH - 1);
-    auto generator = std::bind(distribution, engine);
+
+    std::bitset<LENGTH> bits;
 
     for (unsigned length = 16; length <= LENGTH; length <<= 1)
     {
-        std::bitset<LENGTH> bits;
+        std::uniform_int_distribution<unsigned> distribution(0, length - 1);
+        auto generator = std::bind(distribution, engine);
 
         auto start = std::chrono::high_resolution_clock::now();
 
@@ -25,7 +26,6 @@ int main()
 
         auto stop = std::chrono::high_resolution_clock::now();
 
-        std::cout << "std::bitset, " << length << ", "
-                  << std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() << std::endl;
+        std::cout << "std::bitset, " << length << ", " << std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() << std::endl;
     }
 }
