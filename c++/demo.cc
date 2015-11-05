3678ca8d78523b580ab156bb4c4c456bedf149cf
gustavo <gustavo@ugr.es>
2015-11-05 10:59:13 +0100

    better performance

--- c++/demo.cc
+++ c++/demo.cc
@@ -9,23 +9,20 @@ const unsigned LENGTH     =  32768;
 
 int main()
 {
-    std::random_device rd;
-    std::default_random_engine engine(rd());
-
+    std::default_random_engine engine;
     std::bitset<LENGTH> bits;
 
     for (unsigned length = 16; length <= LENGTH; length <<= 1)
     {
-        std::uniform_int_distribution<unsigned> distribution(0, length - 1);
-        auto generator = std::bind(distribution, engine);
-
         auto start = std::chrono::high_resolution_clock::now();
 
         for (unsigned i = 0; i < ITERATIONS; ++i)
-            bits[generator()].flip();
+            bits[engine() & (length - 1)].flip();
 
         auto stop = std::chrono::high_resolution_clock::now();
 
         std::cout << "std::bitset, " << length << ", " << std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() << std::endl;
     }
+
+    return bits[0]; // avoid code elision
 }
