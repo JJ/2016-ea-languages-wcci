ff4e8ac9fd5b7a062eba47a5424edcfcdacb9d52
Victor M. Rivas Santos <vrivas@ujaen.es>
2015-11-05 22:23:19 +0100

    Adding BitFlip in PHP
    Adding the code, and also the results in vrivas’ Mac

---
+++ php/bitflip.php
@@ -0,0 +1,15 @@
+<?php
+    $ITERATIONS = 100000;
+    $MAXLENGTH = 32768;
+    
+    for( $l=16; $l<=$MAXLENGTH; $l*=2 ) {
+        $b="";
+        for( $i=0; $i<$l; ++$i ) $b.=(rand()/getrandmax()<0.5)?"0":"1";
+        $start=microtime(true);
+        for( $i=0; $i<$ITERATIONS;++$i) {
+            $b[$aleat=rand(0,$l-1)]=($b[$aleat]=="0")?"1":"0";
+        }
+        $end=microtime(true);
+        echo "PHP-bitflip: ".strlen($b).", ".($end-$start)."\n";
+    }
+    ?>
