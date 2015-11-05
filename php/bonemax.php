f00fa90276877a6d99cb56f88b9584dd42ca8968
Victor M. Rivas Santos <vrivas@ujaen.es>
2015-11-05 23:06:06 +0100

    Adding binary One Max in PHP
    According to the code sent bay Pedro Castillo by e-mail:
    
    #include <random>
    #include <time.h>
    
    #include <iostream>
    using namespace std;
    #include "EOBin.h"
    
    #include "EOBinOp.h"
    #include <sys/time.h>
    
    typedef EOBin<float> Chrom;
    
    //----------------------------------------------------------------------
    -------
    
    int main() {
      EOBitFlip<float> bitflip;
    
      unsigned length = 16;
      unsigned iterations = 100000;
      unsigned top_length = 65536;
      do {
        cout << "C++-BitString, " << length << ", ";
    
        struct timeval t1, t2;
        gettimeofday(&t1, NULL);
    
        for (unsigned i = 0; i < iterations; i++ ) {
          Chrom chrom("", length, 1); chrom.initialize();
          unsigned ones = 0;
          for (unsigned j=0; j<chrom.length(); j++ ){
            ones += (chrom[j])?1:0;
          }
        }
    
        gettimeofday(&t2, NULL);
        double elapsedTime = (t2.tv_sec - t1.tv_sec);
        elapsedTime += (t2.tv_usec - t1.tv_usec) / 1e6;   // us to s
        cout <<  elapsedTime << endl;
    
        length = length * 2 ;
      } while (length <= top_length);
    
      return 0;
    }

---
+++ php/bonemax.php
@@ -0,0 +1,16 @@
+<?php
+    $ITERATIONS = 100000;
+    $MAXLENGTH = 65536;
+    
+    for( $l=16; $l<=$MAXLENGTH; $l*=2 ) {
+        $start=microtime(true);
+        for( $i=0; $i<$ITERATIONS;++$i) {
+            $b="";
+            $ones=0;
+            for( $j=0; $j<$l; ++$j ) $b.=(rand()/getrandmax()<0.5)?"0":"1";
+            for( $j=0; $j<$l; ++$j ) $ones+=$b[$j]=="1";
+        }
+        $end=microtime(true);
+        echo "PHP-bOneMax, ".strlen($b).", ".($end-$start)."\n";
+    }
+    ?>
