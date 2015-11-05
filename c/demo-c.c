2935f6c1803ea17dfacc548a7516c5735299749e
JJ Merelo <jjmerelo@gmail.com>
2015-11-05 17:33:22 +0100

    Fixes C

--- c/demo-c.c
+++ c/demo-c.c
@@ -16,11 +16,11 @@ int main()
         clock_gettime(CLOCK_REALTIME, &start);
 
         for (unsigned i = 0; i < ITERATIONS; ++i)
-            bits[rand() & (length - 1)] ^= 1;
+	  bits[rand() & (length - 1)] ^= 1;
 
         clock_gettime(CLOCK_REALTIME, &stop);
 
-        printf("char[], %u, %llu\n", length, (stop.tv_sec - start.tv_sec) * 1000000000ull + (stop.tv_nsec - start.tv_nsec));
+        printf("char[], %u, %f\n", length, (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec)/1e9);
     }
 
     return bits[0]; // avoid code elision
