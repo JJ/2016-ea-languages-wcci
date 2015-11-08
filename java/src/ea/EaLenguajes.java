2d5b9c98492e75420eb1b346f02cc09abb23ae61
JJ Merelo <jjmerelo@gmail.com>
2015-11-08 20:13:17 +0100

    Adds Java

--- java/src/ea/EaLenguajes.java
+++ java/src/ea/EaLenguajes.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ea.lenguajes;
+// package ea.lenguajes;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
