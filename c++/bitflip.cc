c90f2ccb4c97e5cbc5c3370c9bfd7273880322aa
gustavo <gustavo@ugr.es>
2015-12-01 12:56:13 +0100

    changing c++ data structure

--- c++/bitflip.cc
+++ c++/bitflip.cc
@@ -4,27 +4,27 @@
 #include <random>
 
 const unsigned ITERATIONS = 100000;
-const unsigned LENGTH     =  65536;
+const unsigned LENGTH	 =  65536;
 
 int main()
 {
-    std::mt19937 engine;
+	std::mt19937 engine;
 
-    for (unsigned length = 16; length <= LENGTH; length <<= 1)
-    {
-        std::tr2::dynamic_bitset<> bits(length);
+	for (unsigned length = 16; length <= LENGTH; length <<= 1)
+	{
+		std::tr2::dynamic_bitset<> bits(length);
 
-        for (unsigned i = 0; i < length; ++i)
-            bits[i] = engine() & 1;
+		for (unsigned i = 0; i < length; ++i)
+			bits[i] = engine() & 1;
 
-        auto start = std::chrono::high_resolution_clock::now();
+		auto start = std::chrono::high_resolution_clock::now();
 
-        for (unsigned i = 0; i < ITERATIONS; ++i)
-            bits[engine() & (length - 1)].flip();
+		for (unsigned i = 0; i < ITERATIONS; ++i)
+			bits[engine() & (length - 1)].flip();
 
-        auto stop = std::chrono::high_resolution_clock::now();
+		auto stop = std::chrono::high_resolution_clock::now();
 
-        std::chrono::duration<double> t = stop - start;
-        std::cout << "std::tr2::dynamic_bitset<>, " << length << ", " << t.count() << std::endl;
-    }
+		std::chrono::duration<double> t = stop - start;
+		std::cout << "std::tr2::dynamic_bitset<>, " << length << ", " << t.count() << std::endl;
+	}
 }
