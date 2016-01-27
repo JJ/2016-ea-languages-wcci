2bc6efa8a2fbbe2c1268672f54d9cf9b0db0a212
M42 <mromang08@gmail.com>
2016-01-27 22:45:40 +0100

    Haskell Onemax

---
+++ haskell/onemax.hs
@@ -0,0 +1,36 @@
+import Data.Sequence hiding (take)
+import Data.Time
+import Data.Foldable hiding (concat)
+import Control.Applicative
+import System.Random
+
+
+iterations = 100000
+
+onemax :: Seq Bool -> Int
+onemax v = Data.Foldable.foldl (\y -> (\x -> if x then y+1 else y)) 0 v
+
+benchmark :: Int -> IO ()
+benchmark n = do
+  -- Random vector generation
+  gen <- newStdGen
+  let rbools = randoms gen :: [Bool]
+  let vector = fromList $ take n rbools
+
+  -- Timing
+  start <- getCurrentTime
+
+  -- Counting
+  let count = onemax vector
+
+  stop <- getCurrentTime
+  let diffTime = diffUTCTime stop start
+
+  -- Printing
+  putStrLn $ concat ["Haskell-Sequence, ", show n, ", ", show diffTime]
+
+main :: IO ()
+main = do
+  sequence $ Prelude.map benchmark ((2^) <$> [4..16])
+  return ()
+
