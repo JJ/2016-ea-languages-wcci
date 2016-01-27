4a86db8bf7fc4bbf0133fcb7fc0b4999a11656e6
JJ Merelo <jjmerelo@gmail.com>
2016-01-27 18:36:16 +0100

    Uploads new values

---
+++ clojure/project.clj
@@ -0,0 +1,9 @@
+(defproject ea-benchmark "0.1.0-SNAPSHOT"
+  :description "A few benchmarks for clojure"
+  :url "http://github.com/geneura-papers"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
+
