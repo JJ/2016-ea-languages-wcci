65335e9c70d1b2da8d703974f9d5da0be70f864f
JJ Merelo <jjmerelo@gmail.com>
2018-07-14 16:53:59 +0200

    Updates labels closes #31

--- ea-perls.R
+++ ea-perls.R
@@ -10,23 +10,31 @@ measures.rr <- read.csv('measures-royalroad.csv',header=TRUE,sep=",",dec=".")
 ## ----results-perl6, cache=FALSE,echo=FALSE-------------------------------
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 colourCount = length(unique(measures.perl6$Version))
-ggplot(measures.perl6,aes(x=length,y=time,colour=factor(Version)))+ geom_line() + geom_point()  + ggtitle("Perl6 language benchmarks: Onemax") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme( legend.text=element_text(size=10))
+ggplot(measures.perl6,aes(x=length,y=time,colour=factor(Version)))+ geom_line() + geom_point()  + ggtitle("Perl6: Onemax") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme( legend.text=element_text(size=10)) + theme(text = element_text(size=20),
+                                                                                                                                                                                                                                                                                                          axis.text.x = element_text(angle=90, hjust=1)) 
+ggsave("img/result-perl6.png", width=8,height=4.5)
 
 ## ----results-mo, cache=FALSE,echo=FALSE----------------------------------
 colourCount = length(unique(measures.mo$languagerepresentation))
-ggplot(measures.mo,aes(x=length,y=time,colour=factor(languagerepresentation)))+ geom_line() + geom_point()  + ggtitle("Evolutionary algorithm language benchmarks: Onemax") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme( legend.text=element_text(size=10))
+ggplot(measures.mo,aes(x=length,y=time,colour=factor(languagerepresentation)))+ geom_line() + geom_point()  + ggtitle("EA benchmarks: Onemax") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme(text = element_text(size=20),
+                                                                                                                                                                                                                                                                                           axis.text.x = element_text(angle=90, hjust=1)) 
 ggsave("img/results-mo.png", width=8,height=4.5)
 
 ## ----results-bf, cache=FALSE,echo=FALSE----------------------------------
 colourCount = length(unique(measures.bf$languagerepresentation))
-ggplot(measures.bf,aes(x=length,y=time,colour=factor(languagerepresentation)))+ geom_line() + geom_point()  + ggtitle("Evolutionary algorithm language benchmarks: Bitflip mutation") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme( legend.text=element_text(size=10))
+ggplot(measures.bf,aes(x=length,y=time,colour=factor(languagerepresentation)))+ geom_line() + geom_point()  + ggtitle("Bitflip benchmarks") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme(text = element_text(size=20),
+                                                                                                                                                                                                                                                                                                     axis.text.x = element_text(angle=90, hjust=1))
 ggsave("img/results-bf.png", width=8,height=4.5)
 
 ## ----results-xo, cache=FALSE,echo=FALSE----------------------------------
 colourCount = length(unique(measures.bf$languagerepresentation))
-ggplot(measures.xo,aes(x=length,y=time,colour=factor(languagerepresentation)))+ geom_line() + geom_point()  + ggtitle("Evolutionary algorithm language benchmarks: Crossover") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme( legend.text=element_text(size=10))
+ggplot(measures.xo,aes(x=length,y=time,colour=factor(languagerepresentation)))+ geom_line() + geom_point()  + ggtitle("EA benchmarks: Crossover") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme(text = element_text(size=20),
+                                                                                                                                                                                                                                                                                              axis.text.x = element_text(angle=90, hjust=1))
+ggsave("img/results-xo.png", width=8,height=4.5)
 
 ## ----results-rr, cache=FALSE,echo=FALSE----------------------------------
 colourCount = length(unique(measures.rr$Version))
-ggplot(measures.rr,aes(x=length,y=time,colour=factor(Version)))+ geom_line() + geom_point()  + ggtitle("Comparing Perl 5 and Perl 6 using the Royal Road Function") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme( legend.text=element_text(size=10))
+ggplot(measures.rr,aes(x=length,y=time,colour=factor(Version)))+ geom_line() + geom_point()  + ggtitle("Royal Road: Perl 5 vs Perl 6") + scale_y_log10()+scale_x_log10() + scale_color_manual(name='Language',values=getPalette(colourCount)) + theme(text = element_text(size=20),
+                                                                                                                                                                                                                                                                                   axis.text.x = element_text(angle=90, hjust=1))
 ggsave("img/results-rr.png", width=8,height=4.5)
+
